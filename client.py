fc737b271ed0aae19b819cb5636c160994b525fe
mohamed <dwd@gmail.com>
2021-04-26 23:23:00 +0200

    chat server client2client

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import socket
 import threading
 
-clisocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clisocket.connect(('localhost', 54321))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('localhost', 54321))
 print('The client has been connected')
 
 def RecvHandler():
     while True:
-        print('The message from server.', clisocket.recv(256).decode())
+        print('The message from server.', s.recv(256).decode())
 threading._start_new_thread( RecvHandler,())
 
 while True:
     msg = input('Please write the message :')
-    clisocket.send(bytes(msg, encoding='utf8'))
+    s.send(bytes(msg, encoding='utf8'))
--- server.py
+++ server.py
@@ -1,36 +1,29 @@
-import socket
 import threading
+import socket
 
-srvsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Server socket opened')
 
-srvsocket.bind(('localhost', 54321))
+s.bind(('localhost', 54321))
 print('Bind to the local port')
 
-srvsocket.listen(5)
+s.listen(5)
 print('Started listening...')
-
-#New Client Recive Handler
-def NewClientRecvHandler(cli,):
-    print('The new client has socket id:', cli)
+clients=[]
+#New user connect Handler
+def connectnewuser(c,ad):
     while True:
-        print('The message got for client socket:', cli)
-        print(cli.recv(256).decode())
-
-#New Client send Handler
-def NewClientSendHandler(cli,):
-    print('The new client has socket id:', cli)
-    while True:
-        inputdata=input('data to send:')
-        print('The message sent to client socket:', cli)
-        cli.send(bytes('sending:'+inputdata, encoding='utf8'))
-
+        print('The message got for client socket:', c)
+        m=c.recv(256).decode()
+        m=(ad[0]+':'+m)
+        sendtoall(m,c)
+def sendtoall(m,c):
+    for client in clients:
+        if client !=c:
+            client.send(m.encode('utf-8'))
 while True:
-    print('Waiting for the incoming connections..')
-    cli, ip = srvsocket.accept()
-    cli.send(bytes('CONNECT_SUCCESSFUL', encoding='utf8'))
+    c,ad=s.accept()
+    clients.append(c)
+    threading._start_new_thread(connectnewuser,(c,ad))
 
-    #Start the new client thread
-    threading._start_new_thread( NewClientRecvHandler, (cli,))
-    threading._start_new_thread( NewClientSendHandler, (cli,))
-    
+
